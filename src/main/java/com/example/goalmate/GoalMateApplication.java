98c14834f2842f56ac30fc2f17876651e7a1b1c8
darynaitkazy <65250707+darynaitkazy@users.noreply.github.com>
2023-03-10 23:02:27 +0600

    Created project

---
+++ src/main/java/com/example/goalmate/GoalMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.goalmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/goalmate/GoalMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goalmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
